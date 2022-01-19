2cd104569f190ded52ee41d07f39c45f935a2c0f
unknown <aleksanderf997@wp.pl>
2022-01-19 20:01:33 +0100

    updated css

--- components/Success.js
+++ components/Success.js
@@ -1,12 +1,13 @@
+import Link from 'next/link'
 import styles from '../styles/Success.module.css'
 
 function Success() {
     return (
         <div className={styles.successPage}>
             <h1>Zakup udany.</h1>
-            <a href='/'><div className={styles.backBtn}>
+            <Link href='/'><div className={styles.backBtn}>
                 <h1>Powrót</h1>
-            </div></a>
+            </div></Link>
         </div>
     )
 }
